2e72b66c2310bc0b392e9230619e6cbbd95f8a43
fernandobouchet <fernandobouchet@gmail.com>
2023-01-02 18:00:10 -0300

    Enabled cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,13 @@ import 'dotenv/config';
 import express from 'express';
 import connectDB from './database/db';
 import imageRoute from './routes/imageRoute';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 connectDB();
