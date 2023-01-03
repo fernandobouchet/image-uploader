36da9df057586a41576b67ec80df3a1234e64b01
fernandobouchet <fernandobouchet@gmail.com>
2023-01-02 21:39:10 -0300

    Added 'Loader' component

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,40 +1,58 @@
+import { useEffect, useState } from 'react';
 import check from '../assets/check.svg';
+import Loader from './Loader';
 
 type Props = {
   imageUrl: string;
 };
 
 const Card: React.FC<Props> = ({ imageUrl }) => {
+  const [imageIsLoaded, setImageIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const image = new window.Image();
+    image.src = imageUrl;
+    image.onload = () => {
+      setImageIsLoaded(true);
+    };
+  }, [imageUrl]);
+
   return (
-    <div className="container">
-      <div className="check-success">
-        <img src={check} alt="" />
-        <h2>Upload Successfully!</h2>
-      </div>
-      <img
-        className="card-img"
-        src={imageUrl}
-        alt=""
-        width={340}
-        height={225}
-      />
-      <div className="link-container">
-        <input
-          className="link-input"
-          type="text"
-          defaultValue={imageUrl}
-          disabled
-        />
-        <button
-          className="link-button"
-          onClick={() => {
-            navigator.clipboard.writeText(imageUrl);
-          }}
-        >
-          Copy link
-        </button>
-      </div>
-    </div>
+    <>
+      {imageIsLoaded ? (
+        <div className="container">
+          <div className="check-success">
+            <img src={check} alt="" />
+            <h2>Upload Successfully!</h2>
+          </div>
+          <img
+            className="card-img"
+            src={imageUrl}
+            alt=""
+            width={340}
+            height={225}
+          />
+          <div className="link-container">
+            <input
+              className="link-input"
+              type="text"
+              defaultValue={imageUrl}
+              disabled
+            />
+            <button
+              className="link-button"
+              onClick={() => {
+                navigator.clipboard.writeText(imageUrl);
+              }}
+            >
+              Copy link
+            </button>
+          </div>
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </>
   );
 };
 export default Card;
---
+++ frontend/src/components/Loader.tsx
@@ -0,0 +1,11 @@
+const Loader = () => {
+  return (
+    <div className="loading-container">
+      <h2>Uploading...</h2>
+      <div className="loading-bar">
+        <div className="bar" />
+      </div>
+    </div>
+  );
+};
+export default Loader;
